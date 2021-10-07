9253bb8740320839bf0bf3c166b596292d1641d6
Lucas <lucasbrubach@gmail.com>
2021-10-07 14:33:08 -0300

    Variaveis

--- Variaveis/TipoChar.c
+++ Variaveis/TipoChar.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 //A variavel do tipo char ela só suporta 1 caractere ou seja uma letra por variavel
-//
+
 int main(){
-
-    printf("Codado por Lucasbr2k14\n");
-    print("aa");
-
+    char letra = "a"; 
+    printf("\n %d", letra);
     return 0;
 }
---
+++ Variaveis/aritmetica.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// Soma +
+// Subtrção -
+// Multiplicação *
+// Divisão /
+//
+//
+//
+
+int main(){
+    int a,b,res;
+
+    a=1;
+    b=3;
+    
+    res = a+b;//Somando os valores 
+    printf("%d \n", res);
+
+    res = a-b;//Subtraindo os valores
+    printf("%d \n", res);
+    
+    res = a/b;//Dividindo
+    printf("%d \n", res);
+    
+    res = a*b;//Multiplicandop
+    printf("%d \n", res);
+
+    return 0;
+}
