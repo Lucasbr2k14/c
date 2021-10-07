5a12a52860a59fd498665f13e919d578536519de
Lucas <lucasbrubach@gmail.com>
2021-10-06 21:59:50 -0300

    Tipo char

---
+++ Variaveis/TipoChar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//A variavel do tipo char ela só suporta 1 caractere ou seja uma letra por variavel
+//
+int main(){
+
+    printf("Codado por Lucasbr2k14\n");
+    print("aa");
+
+    return 0;
+}
